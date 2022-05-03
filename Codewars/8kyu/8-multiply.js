70e21f87f9cf35a1eef4e433f2ef7f55c9a1b149
molockss <mahamed_67@hotmail.com>
2022-05-03 15:40:14 +0100

    multiply two numbers

---
+++ Codewars/8kyu/8-multiply.js
@@ -0,0 +1,10 @@
+// Implement a function which multiplies two numbers.
+
+function multiply (a,b){
+
+    return a*b;
+}
+    
+// Using arrow functions, this is the answer
+
+let multiply = (a,b) => a*b;
