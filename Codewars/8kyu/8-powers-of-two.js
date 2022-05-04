a62692852db1033b7657328ecc6f0e949bceca1c
molockss <mahamed_67@hotmail.com>
2022-05-04 16:05:47 +0100

    powers of two

---
+++ Codewars/8kyu/8-powers-of-two.js
@@ -0,0 +1,17 @@
+
+   
+//Powers of Two
+/* Complete the function that takes a non-negative 
+integer n as input, and returns a list of all the powers of 2
+with the exponent ranging from 0 to n ( inclusive ) */
+
+
+function powersOftwo () {
+
+    var myArray = []
+    for (var i=0; i<=n; i++){
+        myArray.push(2**i)
+    
+    }
+    return myArray
+}
