deb4aa3edfe3838e3eb72a51d0f7611b946b16ef
molockss <mahamed_67@hotmail.com>
2022-05-05 16:09:22 +0100

    Return quarter based on month

---
+++ Codewars/8kyu/8-quarter-of-year.js
@@ -0,0 +1,6 @@
+// Quarter of the year
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+const quarterOf = month => Math.ceil(month/3);
+
