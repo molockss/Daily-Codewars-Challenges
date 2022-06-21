24feb7aacee48bd43ed411b43a62004acfc8a402
molockss <mahamed_67@hotmail.com>
2022-06-21 19:15:22 +0100

    updated all commits

--- Codewars/8kyu/8-improving-math-round.js
+++ Codewars/8kyu/8-improving-math-round.js
@@ -5,4 +5,8 @@ Math.roundTo = function (number, precision) {
     return + (Math.round (number + "e+" + precision) + "e-" + precision)
     }
 
-    
+    // or you could of done this - much easier
+
+    Math.roundTo = (number, precision) => Number(number.toFixed(precision));
+
+    //. toFixed method (then enter number of decimal places)
