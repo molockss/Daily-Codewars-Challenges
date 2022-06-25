633f7defdbb7e660dcd55a6d5845da11bdabef2d
molockss <mahamed_67@hotmail.com>
2022-06-25 21:13:24 +0100

    string template

---
+++ Codewars/8kyu/8-string-templates.js
@@ -0,0 +1,9 @@
+// Build String template
+
+const buildString = (...template) => `I like ${template.join(', ')}!`;
+
+
+// function buildString (template) {
+
+//     return `I like ${template.join (', ')}!`
+// }
