347cb496a6a787f7314a2d128ff5a8859c21a60f
molockss <mahamed_67@hotmail.com>
2022-07-25 10:17:58 -0400

    count divisors of a number

---
+++ Codewars/7kyu/7-divisors-of-number.js
@@ -0,0 +1,11 @@
+// Count the divisors of a number
+
+function getDivisorsCnt(n){
+    let output = 0; 
+    for (let i=1; i<=n; i++) {
+      if (n % i == 0) {
+        output += 1; 
+}
+}
+  return output;
+}
