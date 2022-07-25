28e4de69be479445d28a0fd818e0defc1384ebce
molockss <mahamed_67@hotmail.com>
2022-07-25 10:24:21 -0400

    count divisors

--- Codewars/7kyu/7-divisors-of-number.js
+++ Codewars/7kyu/7-divisors-of-number.js
@@ -9,3 +9,5 @@ function getDivisorsCnt(n){
 }
   return output;
 }
+
+// so basically the i++ starts the iteration again as long as its less than n, if n is divisable by the new i value, we add it to the output which is 0 and then display it
