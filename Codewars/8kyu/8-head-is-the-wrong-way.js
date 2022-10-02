bdaa0f25358cb0af37bad11dda772d514622accf
molockss <mahamed_67@hotmail.com>
2022-10-02 15:17:01 +0400

    reverse of array

---
+++ Codewars/8kyu/8-head-is-the-wrong-way.js
@@ -0,0 +1,25 @@
+// Re-arrange array
+
+/* it is your job to re-arrange the array so that the animal is the right way round (head, body, tail). 
+
+Example below;
+
+("Testing for fixed tests", () => {
+ ["tail", "body", "head"])
+
+ should equal
+ 
+ ["head", "body", "tail"]);
+
+  
+ Solution:  */
+
+
+ function fixTheMeerkat(arr) {
+    const reversed = arr.reverse();
+    return reversed 
+   }
+
+// I set the variablee reversed equal to the reverse of whateever the array is
+
+// I then returned the variable
