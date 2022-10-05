1f807e7d93a9d6478813c4f9727f629ddc4c3547
molockss <mahamed_67@hotmail.com>
2022-10-05 19:52:07 +0400

    Array of positive integers using prep method

---
+++ Codewars/7kyu/7-sum-of-lowest-positive-integers.js
@@ -0,0 +1,42 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+
+
+//PREP
+
+//Parameters
+
+// array of Positive integers, no more than 4 in the array
+
+
+//Return
+//adding the two lowest integers and returning the sum
+
+
+
+//Examples
+
+// assert.strictEqual(sumTwoSmallestNumbers([5, 8, 12, 19, 22]), 13 , "Sum should be 13");
+// assert.strictEqual(sumTwoSmallestNumbers([15, 28, 4, 2, 43]), 6 , "Sum should be 6");
+// assert.strictEqual(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), 10 , "Sum should be 10");
+// assert.strictEqual(sumTwoSmallestNumbers([23, 71, 33, 82, 1]), 24 , "Sum should be 24");
+// assert.strictEqual(sumTwoSmallestNumbers([52, 76, 14, 12, 4]), 16 , "Sum should be 16");
+// });
+
+
+
+//Pseudocode
+
+//We want to sort through the array and then add the two smallest integers, once we sort it , it will be the first and second element in the array
+
+
+// Solution
+
+function sumTwoSmallestNumbers(numbers) {  
+    let arr = numbers.sort( (a, b) => a - b );  // a-b, put is in order of smallest to largest
+    return arr[0] + arr[1] // this returns the smallest two and adds them together
+  }
+
+  sumTwoSmallestNumbers([5, 8, 12, 19, 22])
