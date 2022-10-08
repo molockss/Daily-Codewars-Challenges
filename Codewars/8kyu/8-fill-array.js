9bb8798638f21dc0d01117bfd83d16294d9f4afa
molockss <mahamed_67@hotmail.com>
2022-10-08 06:49:06 +0400

    complete array challenge

--- Codewars/8kyu/8-fill-array.js
+++ Codewars/8kyu/8-fill-array.js
@@ -23,3 +23,15 @@ const { arrayBuffer } = require("stream/consumers")
 
 // we will use the number as the array.length so up until that number populate. Use one of the methods to push stuff into an array using a for loop
 
+// Solution
+
+function arr(n){
+    var newArr = [];
+    for(var i = 0; i < n; i++){
+      newArr.push(i);  // here we're pushing everything we get back from the loop in the loops. starting from o
+    }
+    return newArr;
+  }
+  
+
+//   so if we call arr(5), we will get 0 first then 1,2,3,4
