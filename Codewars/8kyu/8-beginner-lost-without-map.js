a32026f95bd1629a4acd1e3e86025e40ec2c2551
Mahamed Osman <mahamed_67@hotmail.com>
2022-11-07 19:34:00 +0400

    array.map used

---
+++ Codewars/8kyu/8-beginner-lost-without-map.js
@@ -0,0 +1,25 @@
+/* Beginner - Lost Without a Map
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+
+Solution */
+
+
+
+function maps(x){
+  
+    return x.map ((e,i) => e*2 )
+    
+  }
+  
+  //It calls the function for each element of the array and returns the array of results.
+  // here we are doubling the element and returning it 
+
+
+  
+// const maps = x=> x.map ((e,i) => e*2)
